af5b7ca95b4bcec26e79c98e8b97dc1ff4c41394
TomChambers <tc262389@falmouth.ac.uk>
2023-01-06 12:46:16 +0000

    Day two first task working

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
-def DayOne():
+def DayOne(input):
 
-    with open("dayOneInput.txt") as dayOneInput:
+    with open(input) as dayOneInput:
         # Lines of input and individual elfs cals
         lines = []
         elfCal = []
@@ -35,9 +35,60 @@ def DayOne():
             del(elfCal[location])
 
         return (stack[-1] + stack[-2] + stack[-3])
+def DayTwo(input):
+    with open(input) as daysInput:
+        # ROCK PAPER SCISSORS
+        opp = ['A','B','C']
+        player = ['X','Y','Z']
+        points = { "player" : 6, "opp" : 0, "draw" : 3}
+        rounds = []
+        score = 0
 
+        # Read lines and remove \n
+        for line in daysInput:
+            rounds.append(line[:-1])
 
+        # Calculate score for each round and add it to the running total for the player
+        for round in rounds:
+            oppTurn = opp.index(round[0])
+            playerTurn = player.index(round[2])
+            total = oppTurn + playerTurn
+
+            # Calculate the result
+            # ROCK 1
+            # PAPER 2
+            # SCISSORS 3
+            #
+            #
+            # 0 + 1 = 1
+            # 0 + 2 = 2
+            # 1 + 2 = 3
+
+            if total == 2*oppTurn:
+                result = 'draw'
+            elif total == 1:
+                if oppTurn == 0:
+                    result = 'player'
+                else:
+                    result = 'opp'
+            elif total == 2:
+                if oppTurn == 0:
+                    result = 'opp'
+                else:
+                    result = 'player'
+            else:
+                if oppTurn == 1:
+                    result = 'player'
+                else:
+                    result = 'opp'
+
+            if result != "opp":
+                score += points[result]
+            score += playerTurn+1
+
+        return(score)
 
 if __name__ == '__main__':
-    print(DayOne())
+    #print(DayOne("dayOneInput.txt"))
+    print(DayTwo("dayTwoInput.txt"))
 
