fc7703a53028bf291ecfc334a589432904a6514f
TomChambers <tc262389@falmouth.ac.uk>
2023-01-06 11:53:11 +0000

    Day one boths tasks working

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def DayOne():
+
+    with open("dayOneInput.txt") as dayOneInput:
+        # Lines of input and individual elfs cals
+        lines = []
+        elfCal = []
+
+        # Read input lines into lines[]
+        for line in dayOneInput:
+            if line == '\n':
+                lines.append(-1)
+            else:
+                lines.append(int(line[:-1]))
+
+        # Find total of individual elf cals and load them into elfCal
+        runningTotal = 0
+        lines.append(-1)
+        for el in lines:
+            if el > -1:
+                runningTotal += el
+            else:
+                elfCal.append(runningTotal)
+                runningTotal=0
+
+        # Find the top 3 elfs with largest cal total and return it
+        stack = []
+        for i in range(3):
+            largestTotal = 0;
+            for j in range(0,len(elfCal)):
+                if elfCal[j] > largestTotal:
+                    largestTotal = elfCal[j]
+                    location = j
+            stack.append(largestTotal)
+            del(elfCal[location])
+
+        return (stack[-1] + stack[-2] + stack[-3])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(DayOne())
 
